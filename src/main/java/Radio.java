d3074c3aad8033020bce1ef71140dc7a33d74b14
Yana Kalugina <yanakaluginayana@gmail.com>
2022-08-07 15:41:01 +0300

    Third commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,9 +1,18 @@
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int stationsCount;
+
+    public Radio() {
+        this.stationsCount = 10;
+    }
+
+    public Radio(int stationsCount) {
+        this.stationsCount = stationsCount;
+    }
 
     public void nextStation() {
-        if (currentStation < 9) {
+        if (currentStation < stationsCount - 1) {
             currentStation++;
         } else {
             currentStation = 0;
@@ -14,49 +23,41 @@ public class Radio {
         if (currentStation > 0) {
             currentStation--;
         } else {
-            currentStation = 9;
+            currentStation = stationsCount - 1;
         }
     }
 
     public void nextVolume() {
-        if (currentVolume < 10) {
+        if (currentVolume < 100) {
             currentVolume++;
-        } else {
-            currentVolume = 0;
         }
     }
 
     public void prevVolume() {
         if (currentVolume > 0) {
             currentVolume--;
-        } else {
-            currentVolume = 10;
         }
     }
 
-    public int getCurrentStation() {
-        return currentStation;
-    }
+    public int getCurrentStation() {return currentStation;}
 
     public void setCurrentStation(int currentStation) {
         if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > stationsCount - 1) {
             return;
         }
         this.currentStation = currentStation;
     }
 
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
+    public int getCurrentVolume() {return currentVolume;}
 
     public void setCurrentVolume(int currentVolume) {
         if (currentVolume < 0) {
             return;
         }
-        if (currentVolume > 10) {
+        if (currentVolume > 100) {
             return;
         }
         this.currentVolume = currentVolume;
